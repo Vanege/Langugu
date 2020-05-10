4dfaaf5ca55bbe54fe6738f079ee4b8ee7c45152
Vanege <11367082+Vanege@users.noreply.github.com>
2020-05-10 11:42:35 +0200

    naada ti gramari partu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,29 +5,14 @@ module.exports = {
   themeConfig: {
     logo: '/img/logo.png',
     nav: [
-      { text: 'Kurso', link: '/Kurso/Kurso/'},
-      { text: 'Gramatiko', link: '/Gramatiko/Prezento/' },
-      { text: 'Vikio', link: 'https://langugu.fandom.com/wiki/Langugu' },
+      { text: 'Kurso', link: '/Kurso/'},
       { text: 'Korektilo', link: 'https://vanege.github.io/langugi-korekteru/' },
+      { text: 'Vikio', link: 'https://langugu.fandom.com/wiki/Langugu' },
       { text: 'Komunumo', link: 'https://langugu.telegramo.org' },
     ],
     sidebar: {
-      '/Gramatiko/': [
-        ['Prezento', 'Prezento'],
-        ['Literoj kaj sonoj', 'Literoj kaj sonoj'],
-        ['Vorto', 'Vorto'],
-        ['Substantivo', 'Substantivo (-u)'],
-        ['Modifilo', 'Modifilo (-i)'],
-        ['Verbo', 'Verbo (-a)'],
-        ['Rolvorteto', 'Rolvorteto (-o)'],
-        ['Tabelvorto', '"Tabelvorto"'],
-        ['Rilata subfrazo', 'Rilata subfrazo'],
-        ['Participo', '"Participo"'],
-        ['Kunmetado', 'Kunmetado'],
-        ['Algoritmo', 'Algoritmo']
-      ],
       '/Kurso/': [
-        ['Kurso', 'Kurso']
+        ['', 'Kurso'] 
       ]
     }
   }
